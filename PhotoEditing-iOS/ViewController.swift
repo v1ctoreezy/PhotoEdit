ddeee660179be613c0187525faee0fde01a6b0ff
Victor Cherkasov <solidtechniquee@gmail.com>
2025-02-06 21:09:39 +0500

    Initial Commit

---
+++ PhotoEditing-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoEditing-iOS
+//
+//  Created by Victor Cherkasov on 06.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
